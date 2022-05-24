6ee3349319472a289b208566a28c5200cf0bb014
roman <guseynoff.roma@yandex.ru>
2022-05-24 14:07:45 +0300

    Home work lesson_1

--- mainapp/views.py
+++ mainapp/views.py
@@ -14,7 +14,7 @@ class NewsPageView(TemplateView):
         # Get all previous data
         context = super().get_context_data(**kwargs)
         # Create your own data
-        context["news_title"] = "Громкий новостной заголовок"
+        context["news_title"] = "Громкий новостной заголовок" 
         context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
         context["range"] = range(5)
         context["datetime_obj"] = datetime.now()
